f5333df46f1c24779532c0c088d4bdbff2611dd9
王雪梅 <wxm112@me.com>
2015-02-02 12:50:34 +1100

    first commit

---
+++ intro/app/controllers/butterflies_controller.rb
@@ -0,0 +1,13 @@
+class ButterfliesController < ApplicationController
+
+    def index
+    end
+
+    def new
+        redirect_to('/butterflies')
+    end
+
+    def faq
+        render 'pages/faq'
+    end
+end

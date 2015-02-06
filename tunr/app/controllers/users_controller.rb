ac22594e821eb836220b6cd82f9349073024d599
王雪梅 <wxm112@me.com>
2015-02-06 14:13:31 +1100

    added tunr

--- tunr/app/controllers/users_controller.rb
+++ tunr/app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :check_if_admin, :only => [:index]
+
   def new
     @user = User.new
   end
@@ -6,16 +8,25 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-        redirect_to root_path
+      redirect_to ro_path
     else
-        render :new
+      render :new
     end
   end
+
+  def index
+    @users = User.all
+  end
+
 
 
   private
   def user_params
     params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+
+  def check_if_admin
+    redirect_to(root_path) unless @current_user.present? and @current_user.is_admin?
   end
 
 end
--- tunr/config/routes.rb
+++ tunr/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root :to => 'pages#home'
-  get '/home' => 'pages#home'
+  resources :users, :only => [:new, :create, :index]
 
-  resources :users, :only => [:new, :create]
+
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  delete '/login' => 'session#destroy'
 end
